237d0059a5f9c49e6300fd9bc249c1c2248db782
thegeez <thegeez@users.noreply.github.com>
2015-10-22 15:54:32 +0200

    more debug cljs

--- src/cljs/net/thegeez/gatherlist/client.cljs
+++ src/cljs/net/thegeez/gatherlist/client.cljs
@@ -13,15 +13,20 @@
 (def updates (atom 0))
 
 (defn on-event [event]
+  (println "on-event")
   (let [count (swap! updates inc)]
     (when (= count 1)
+      (println "remove class")
       (classes/remove updates-elem "hidden"))
+    (println "set text content" count)
     (gdom/setTextContent updates-elem
                          (str count " new items available"))))
 
 (defn ^:export startstream [url]
+  (println "start stream from " url)
   (event-source/event-source url
                              :on-message (fn [event]
+                                           (println "EventStream message: " event)
                                            (on-event event))
                              :on-error (fn [e]
                                          (println "EventStream error: " e))))
